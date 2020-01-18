d52fbb2edb930843db5229a58a0ea444f4d83f3b
Mystic67 <webmaster@passion-geek.com>
2020-01-18 01:24:49 +0100

    Added Email backend for development mode.

--- pur_beurre_V2_app/settings/__init__.py
+++ pur_beurre_V2_app/settings/__init__.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'store.apps.StoreConfig',
     'accounts.apps.AccountsConfig',
 ]
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # During development only
 
 AUTH_USER_MODEL = 'accounts.User'
 
