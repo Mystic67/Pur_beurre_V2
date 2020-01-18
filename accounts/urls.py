46bc650d3d3f7728612b0a71c3bf1fcdcacea79c
Mystic67 <webmaster@passion-geek.com>
2020-01-18 01:26:48 +0100

    Added Urls for password reset.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
-from django.urls import path, re_path
+from django.contrib.auth import views as auth_views
+from django.urls import path, re_path, reverse_lazy
 from . import views
 
 app_name = "accounts"
@@ -10,4 +11,21 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.user_register, name='register'),
     path('myaccount/', views.user_account, name='myaccount'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html',
+        email_template_name='accounts/password_reset_email.html',
+        subject_template_name='accounts/password_reset_subject.txt',
+        success_url=reverse_lazy('accounts:password_reset_done')
+    ), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('password_reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html',
+        success_url=reverse_lazy('accounts:password_reset_complete')
+    ), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
+
 ]
